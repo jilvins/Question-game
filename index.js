92875a0aeca1e4fa70440eaf4d105b10198e1d6a
jilvins <59861669+jilvins@users.noreply.github.com>
2020-11-17 20:17:44 +0200

    First publication
    Question game at the beggining stage of development.
    I am expecting to add a lot of additional questions as I will advence in my Javascript learning path.

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const atb = document.getElementById('atbilde')
+const image = document.getElementById('oponentImage')
+const imageBody = document.getElementById('imageBody')
+const questionList = ['When did time begin?', 'Is there life after death?', 'What is the goal of humanity?', 'Why does anything exist?']
+const questions = document.getElementById('yourText')
+
+function showQuestion() {
+    let randomNumber = Math.floor(Math.random() * questionList.length)
+    let randomQuestion = questionList[randomNumber]
+    const newDiw = document.createElement('p')
+    newDiw.setAttribute ('id', 'jaut5')
+    newDiw.innerText = randomQuestion
+    questions.appendChild(newDiw)    
+}
+showQuestion()
+
+document.getElementById('jaut1').addEventListener('click', function() {
+    atb.textContent = "You are welcome!"
+})
+document.getElementById('jaut2').addEventListener('click', function() {
+    image.src='/images/smile-3233682_640.jpg'
+})
+document.getElementById('jaut3').addEventListener('click', function() {
+    imageBody.style.background = 'black'
+})
+document.getElementById('jaut4').addEventListener('click', function renderFriend() {
+    const newFriend = document.createElement('IMG')
+    newFriend.setAttribute('src', '/images/smiley-4832530_640.png') 
+    newFriend.setAttribute('width', '250')
+    newFriend.setAttribute('height', '250')
+    imageBody.appendChild(newFriend) 
+})
+document.getElementById('jaut5').addEventListener('click', function() {
+    atb.textContent = "God only knows!"
+})
